c3de4f05a9455c39be7b51269e29b01bb932cf97
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-24 21:52:42 +0300

    fix: update footer item font size, adjust root padding, and correct navbar link href

--- src/js/header.js
+++ src/js/header.js
@@ -3,12 +3,19 @@ const navbarToggleIcon = document.querySelector('#navbar-toggle-icon');
 
 const checkActivePageLink = () => {
     const activePageLink = window.location.href.split('/').pop();
-    document.querySelectorAll('.Navbar__content-item a').forEach(link => {
+    console.log(activePageLink)
+    const links = document.querySelectorAll('.Navbar__content-item a');
+    if (activePageLink === '' || activePageLink === ' ') {
+        console.log(links[0])
+        links[0].parentElement.classList.add('active');
+    }
+    links.forEach(link => {
         const linkHref = link.getAttribute('href');
 
         if (activePageLink === linkHref) {
             link.parentElement.classList.add('active');
         }
+
     });
 }
 
