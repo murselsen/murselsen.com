2f50ccca6d4fb45d51ff50196654ef35f44fd7ab
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-30 13:14:42 +0300

    Updates

---
+++ about.js
@@ -0,0 +1,2 @@
+import"./assets/main-CTSX9I4L.js";import"./assets/vendor-BuhHjXTg.js";const l=document.querySelectorAll(".accordion");console.log(l);l.forEach(e=>{console.log(e),console.log(e.parentNode.children),e.addEventListener("click",()=>{const o=e.parentNode.children[1];o.nodeName==="UL"&&o.classList.toggle("active")})});
+//# sourceMappingURL=about.js.map
