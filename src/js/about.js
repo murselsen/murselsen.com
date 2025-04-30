4fec60edca5e71924cf830cf6be278e254a26cb6
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-30 14:49:53 +0300

    fix: correct spelling in index.html and update accordion item titles in about.html style: adjust CSS for sidebar accordion display and add text transformation

--- src/js/about.js
+++ src/js/about.js
@@ -2,11 +2,6 @@ const allAccordion = document.querySelectorAll('.accordion');
 console.log(allAccordion);
 
 allAccordion.forEach((accordion) => {
-    console.log(accordion);
-    console.log(accordion.parentNode.children)
-
-
-
     accordion.addEventListener('click', () => {
         const list = accordion.parentNode.children[1]
         if (list.nodeName === "UL") {
