7b9e27b549cfcfac1f08bf0b8a9f48a898651e05
Mürsel SEN <murselsen_35@hotmail.com>
2025-05-01 17:52:48 +0300

    Updates

--- about.js
+++ about.js
@@ -1,2 +1,2 @@
-import"./assets/main-arGbGiWB.js";import"./assets/vendor-p-D2snow.js";const c=document.querySelectorAll(".accordion");console.log(c);c.forEach(e=>{e.addEventListener("click",()=>{const o=e.parentNode.children[1];o.nodeName==="UL"&&o.classList.toggle("active")})});
+import"./assets/main-CfnU0q-h.js";import"./assets/vendor-p-D2snow.js";const c=document.querySelectorAll(".accordion");console.log(c);c.forEach(e=>{e.addEventListener("click",()=>{const o=e.parentNode.children[1];o.nodeName==="UL"&&o.classList.toggle("active")})});
 //# sourceMappingURL=about.js.map
