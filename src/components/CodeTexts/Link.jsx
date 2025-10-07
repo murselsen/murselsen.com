a006e0cc9b0b0a93a1f15e28885006f62b354cab
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-10-08 01:42:48 +0300

    feat: Yeni Link bileşeni eklendi

---
+++ src/components/CodeTexts/Link.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles/Link.module.css";
+
+const Link = ({ value }) => {
+  return (
+    <a href={value} target="_blank" className={styles.value}>
+      "{value}"
+    </a>
+  );
+};
+
+export default Link;
