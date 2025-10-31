f0ece77b59954a2a5ce681a361c544c75ba6e3c1
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-10-31 12:17:45 +0300

    style: Improve code formatting and consistency in Projects and slice files

--- src/redux/github/slice.js
+++ src/redux/github/slice.js
@@ -1,32 +1,31 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 // Thunks
-import {fetchGistById, fetchProfileGithub, fetchRepos} from "./thunk.js";
+import { fetchGistById, fetchProfileGithub, fetchRepos } from "./thunk.js";
 
 const initialState = {
-    username: "murselsen",
-    profile: null,
-    gists: [],
-    repos: []
+  username: "ozncncyr",
+  profile: null,
+  gists: [],
+  repos: [],
 };
 
 const githubSlice = createSlice({
-    name: 'github',
-    initialState,
-    extraReducers: (builder) => {
-        builder.addCase(fetchProfileGithub.fulfilled, (state, action) => {
-            state.profile = action.payload;
-        })
-            .addCase(fetchGistById.fulfilled, (state, action) => {
-                const gistList = [];
-                gistList.push(action.payload);
-                state.gists = gistList;
-            })
-            .addCase(fetchRepos.fulfilled, (state, action) => {
-                state.repos = action.payload;
-            })
-
-    }
-})
-
+  name: "github",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProfileGithub.fulfilled, (state, action) => {
+        state.profile = action.payload;
+      })
+      .addCase(fetchGistById.fulfilled, (state, action) => {
+        const gistList = [];
+        gistList.push(action.payload);
+        state.gists = gistList;
+      })
+      .addCase(fetchRepos.fulfilled, (state, action) => {
+        state.repos = action.payload;
+      });
+  },
+});
 
 export default githubSlice.reducer;
